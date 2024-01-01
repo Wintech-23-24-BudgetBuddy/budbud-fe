ce65385a7551d5d6650bc5e660ec6cde881da128
LiYuan <lywong113@gmail.com>
2024-01-01 16:36:58 +0800

    feat: add chatbot fe

---
+++ src/pages/Chatbot/Message.js
@@ -0,0 +1,36 @@
+import React from "react";
+import { Box, Paper, Typography } from "@mui/material";
+import PropTypes from 'prop-types';
+
+
+
+
+export const Message = ({ message }) => {
+    const isBot = message.sender === "bot";
+  
+    return (
+      <Box
+        sx={{
+          display: "flex",
+          justifyContent: isBot ? "flex-start" : "flex-end",
+          mb: 2,
+        }}
+      >
+        <Paper
+          variant="outlined"
+          sx={{
+            p: 1,
+            backgroundColor: isBot ? "primary.light" : "secondary.light",
+          }}
+        >
+          <Typography variant="body1">{message.text}</Typography>
+        </Paper>
+      </Box>
+    );
+  };
+
+  Message.propTypes = {
+    message: PropTypes.object.isRequired,
+};
+
+export default Message;
