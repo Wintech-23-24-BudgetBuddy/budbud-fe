d36e6aad98ef0a7fe7a7f6cb29254dd987cc749f
jolyn <134131301+jolynleow2301@users.noreply.github.com>
2024-01-07 18:29:34 +0800

    Create app.js

---
+++ app.js
@@ -0,0 +1,31 @@
+import React from 'react';
+import List from './accounts';
+
+function App() {
+  return (
+    <div>
+      <list type='January'>
+        <li>Savings</li>
+        <li>Spendings</li>
+      </list>
+      <list type= 'February'>
+        <li>Savings</li>
+        <li>Spendings</li>
+      </list>
+      <list type= 'March'>
+        <li>Savings</li>
+        <li>Spendings</li>
+      </list>
+      <list type= 'March'>
+        <li>Savings</li>
+        <li>Spendings</li>
+      </list>
+      <list type= 'April'>
+        <li>Savings</li>
+        <li>Spendings</li>
+      </list>
+    </div>
+  );
+}
+
+export default App;
