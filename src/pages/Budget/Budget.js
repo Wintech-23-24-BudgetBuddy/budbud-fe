d85a4bc92afbf0d3e90344001c3e95892679b56c
LiYuan <lywong113@gmail.com>
2024-01-14 14:55:38 +0800

    feat: implement budget page

--- src/pages/Budget/Budget.js
+++ src/pages/Budget/Budget.js
@@ -1,10 +1,45 @@
 import React from 'react'
+import { Box, CssBaseline, CircularProgress, Typography} from '@mui/material'
 
 //Where the user can see their budgets and create new budgets (e.g. for food, for savings, etc.)
 export default function Budget() {
+  const expectedSavings = 1000
+  const actualSavings = 500
+  const expectedSpendings = 1000
+  const actualSpendings = 200
+  const savingProgress = actualSavings/expectedSavings * 100
+  const spendingProgress = actualSpendings/expectedSpendings * 100
+
   return (
-    <div>
-      This is the budget page, its not done yet 🥲
-    </div>
+    <Box sx={{ display: 'flex', pt:10, justifyContent:"center"}}>
+      <CssBaseline />
+      <Box
+        component="main"
+        sx={{display: 'flex', flexDirection:'column', flexGrow: 1, bgcolor: 'background.default', p: 3, alignItems:"center" }}
+      >
+
+        <CircularProgress size={300} variant="determinate" value={savingProgress} />
+        <Typography pt={5} align="center" variant="h4" component="div" >
+          Actual Savings: {actualSavings}
+        </Typography>
+        <Typography pt={2} align="center" variant="h4" component="div" >
+          Expected Savings: {expectedSavings}
+        </Typography>
+        
+      </Box>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1,display: 'flex', flexDirection:'column',  bgcolor: 'background.default', p: 3, alignItems:"center" }}
+      >
+        <CircularProgress size={300} variant="determinate" value={spendingProgress} />
+        <Typography pt={5} align="center" variant="h4" component="div" >
+          Actual Spendings: {actualSpendings}
+        </Typography>
+        <Typography pt={2} align="center" variant="h4" component="div" >
+          Expected Spendings: {expectedSpendings}
+        </Typography>
+        
+      </Box>
+    </Box>
   )
 }
